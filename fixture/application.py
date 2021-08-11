0bc0aaa2ac61b8475ecef9f1b58622c49c0cf14e
Anna <liska.jkl@gmail.com>
2021-08-11 23:00:02 +0300

    Созданы базовые файлы проекта

---
+++ fixture/application.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from fixture.session import SessionHelper
+
+
+class Application:
+
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognised browser %s" % browser)
+        self.wd.implicitly_wait(1)
+        self.session = SessionHelper(self)
+        self.base_url = base_url
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get(self.base_url)
+        return wd
+
+    def destroy(self):
+        self.wd.quit()
+
+    def return_to_home_page(self):
+        wd = self.wd
+        if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_name("group")) > 0):
+            wd.find_element_by_link_text("home page").click()
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
